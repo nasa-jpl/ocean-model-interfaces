3a341f0f8712b6833cdc21690ac371208ac0b7fc
Andrew Branch <andrew.branch@jpl.nasa.gov>
2019-04-18 12:08:25 -0700

    Remove build dir

--- build/gtest/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/branch/projs/ocean_worlds/ocean_models/build/gtest/googletest-src/googletest
-# Build directory: /Users/branch/projs/ocean_worlds/ocean_models/build/gtest/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
